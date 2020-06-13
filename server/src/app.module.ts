b6cd8646cf82a8fb783faeea583dac589dcb8220
Hazumi Yuuri <hazumi.yuuri@gmail.com>
2020-06-13 11:49:40 +0300

    Setup Projet and Packages

---
+++ server/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {}
