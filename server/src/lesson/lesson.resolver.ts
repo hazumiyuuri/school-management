c9bcf4ff912f681f93bcbf129a8932e2a1d1d4a4
Hazumi Yuuri <hazumi.yuuri@gmail.com>
2020-06-13 14:53:57 +0300

    TypeORM and adding creating Mutations for Lesson

--- server/src/lesson/lesson.resolver.ts
+++ server/src/lesson/lesson.resolver.ts
@@ -1,15 +1,33 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+    constructor(
+        private lessonService: LessonService
+    ) {
+
+    }
+
     @Query(returns => LessonType)
-    lesson() {
-        return {
-            id: 'dqsdsqdqsd',
-            name: 'Physics Class',
-            startDate: (new Date()).toISOString(),
-            endDate: (new Date()).toISOString()
-        }
+    lessons() {
+        return this.lessonService.getAllLesson()
+    }
+
+    @Query(returns => LessonType)
+    lesson(
+        @Args('id') id: string,
+    ) {
+        return this.lessonService.getLesson(id);
+    }
+
+    @Mutation(returns => LessonType)
+    createLesson(
+        @Args('name') name: string,
+        @Args('startDate') startDate: string,
+        @Args('endDate') endDate: string
+    ) {
+        return this.lessonService.createLesson(name, startDate, endDate)
     }
 }
